efc34d12f0ff7188139b20cb05fcbc059c33bb8f
Joseph DeWeese <jdeweese@weldtascics.systems>
2023-09-23 14:32:48 -0500

    Initial Commit

---
+++ DeWeese2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeWeese2
+//
+//  Created by J. DeWeese on 9/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DeWeese2/DeWeese2App.swift
@@ -0,0 +1,17 @@
+//
+//  DeWeese2App.swift
+//  DeWeese2
+//
+//  Created by J. DeWeese on 9/23/23.
+//
+
+import SwiftUI
+
+@main
+struct DeWeese2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
